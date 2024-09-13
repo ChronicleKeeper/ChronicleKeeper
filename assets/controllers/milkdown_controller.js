6f400314f3807d49464ede7af943ea7e34e7e1b1
Denis Zunke <denis.zunke@gmail.com>
2024-09-13 15:25:21 +0200

    Cleanup importmap

--- assets/controllers/milkdown_controller.js
+++ assets/controllers/milkdown_controller.js
@@ -1,11 +1,21 @@
 import {Controller} from '@hotwired/stimulus';
-import {Crepe} from '@milkdown/crepe';
+
+import { Crepe } from 'https://cdn.jsdelivr.net/npm/@milkdown/crepe@7.5.7/+esm';
+
 
 export default class extends Controller {
     connect() {
+        console.log('Connect Crepe Editor');
+        console.log(this.element);
+        console.log(this.element.getAttribute('id'));
+
         const crepe = new Crepe({
             root: this.element,
             defaultValue: 'Hello, Milkdown!',
+        });
+
+        crepe.create().then(() => {
+            console.log('Crepe Editor is ready to use!');
         });
     }
 }
