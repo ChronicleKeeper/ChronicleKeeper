11f989db871e0e3f79ad7cab28a50ff661fc16bc
Denis Zunke <denis.zunke@gmail.com>
2024-09-17 22:36:35 +0200

    Remove some old LLM Storage code

--- src/Web/Kernel.php
+++ src/Web/Kernel.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 namespace DZunke\NovDoc\Web;
 
-use DZunke\NovDoc\Domain\LLMExtension\Store\Filesystem\Store as FilesystemStore;
-use PhpLlm\LlmChain\Store\StoreInterface;
-use PhpLlm\LlmChain\Store\VectorStoreInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ChildDefinition;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 use function dirname;
@@ -24,19 +19,5 @@ class Kernel extends BaseKernel
             __DIR__,
             2,
         );
-    }
-
-    protected function prepareContainer(ContainerBuilder $container): void
-    {
-        parent::prepareContainer($container);
-
-        // Register the Filesystem Store as a Vector Store - the bundle is hardly bound to stuff being inside LlmChain
-        $definition = new ChildDefinition(FilesystemStore::class);
-        $definition->setArgument('$filepath', $this->getProjectDir() . '/var/embeddings.json');
-
-        $container->setDefinition('llm_chain.store.filesystem', $definition);
-
-        $container->setAlias(VectorStoreInterface::class, 'llm_chain.store.filesystem');
-        $container->setAlias(StoreInterface::class, 'llm_chain.store.filesystem');
     }
 }
