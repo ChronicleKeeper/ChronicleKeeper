369ce384c599a6618556607701d97faf9ea990ae
Denis Zunke <denis.zunke@gmail.com>
2024-09-27 20:37:34 +0200

    Add modal twig component

---
+++ src/Shared/Twig/Tabler/ModalComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChronicleKeeper\Shared\Twig\Tabler;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('Tabler:Modal', template: 'components/shared/tabler/modal.html.twig')]
+class ModalComponent
+{
+    public string|null $id            = null;
+    public bool $showClosableInHeader = true;
+}
