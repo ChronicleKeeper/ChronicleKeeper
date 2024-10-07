1d6c44b799497645d562f8d277e565828a612c65
Denis Zunke <denis.zunke@gmail.com>
2024-10-07 08:47:39 +0200

    Directory migrator to add a directory to imported files

---
+++ src/Library/Application/Service/Migrator/FixEmptyDirectory.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChronicleKeeper\Library\Application\Service\Migrator;
+
+use ChronicleKeeper\Library\Domain\RootDirectory;
+use ChronicleKeeper\Settings\Application\Service\FileType;
+use ChronicleKeeper\Settings\Application\Service\Migrator\FileMigration;
+use Symfony\Component\Filesystem\Filesystem;
+
+use function array_key_exists;
+use function is_array;
+use function json_decode;
+use function json_encode;
+
+use const JSON_PRETTY_PRINT;
+use const JSON_THROW_ON_ERROR;
+
+final class FixEmptyDirectory implements FileMigration
+{
+    public function __construct(
+        private readonly Filesystem $filesystem,
+    ) {
+    }
+
+    public function isSupporting(FileType $type, string $fileVersion): bool
+    {
+        // Do it on every import for documents and images
+        return $type === FileType::LIBRARY_DOCUMENT
+            || $type === FileType::LIBRARY_IMAGE;
+    }
+
+    public function migrate(string $file): void
+    {
+        $fileContent = $this->filesystem->readFile($file);
+        $jsonArr     = json_decode($fileContent, true, 512, JSON_THROW_ON_ERROR);
+
+        if (
+            ! is_array($jsonArr)
+            || array_key_exists('directory', $jsonArr)
+        ) {
+            // Nothing to do - there is a directory
+            return;
+        }
+
+        $jsonArr['directory'] = RootDirectory::ID;
+
+        $this->filesystem->dumpFile($file, json_encode($jsonArr, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+    }
+}
