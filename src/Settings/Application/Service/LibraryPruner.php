0732b1ae168f25adf57989b7fe88c4fc53ce97f6
Denis Zunke <denis.zunke@gmail.com>
2024-11-12 16:13:40 +0100

    Improve library prune is also removing generated images and favorites

--- src/Settings/Application/Service/LibraryPruner.php
+++ src/Settings/Application/Service/LibraryPruner.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace ChronicleKeeper\Settings\Application\Service;
 
+use ChronicleKeeper\Library\Infrastructure\Repository\FilesystemDocumentRepository;
 use ChronicleKeeper\Shared\Infrastructure\Persistence\Filesystem\PathRegistry;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+
+use const DIRECTORY_SEPARATOR;
 
 class LibraryPruner
 {
@@ -26,6 +29,13 @@ class LibraryPruner
         $this->pruneDirectoryContent($this->pathRegistry->get('vector.documents'));
         $this->pruneDirectoryContent($this->pathRegistry->get('vector.images'));
         $this->pruneDirectoryContent($this->pathRegistry->get('library.conversations'));
+
+        $this->pruneDirectoryContent($this->pathRegistry->get('generator.requests'));
+        $this->pruneDirectoryContent($this->pathRegistry->get('generator.images'));
+
+        $this->pruneDirectoryContent($this->pathRegistry->get('temp'));
+
+        $this->pruneFileFromPath($this->pathRegistry->get('storage'), 'favorites.json');
     }
 
     private function pruneDirectoryContent(string $path): void
@@ -41,5 +51,10 @@ class LibraryPruner
             $this->filesystem->remove($filePath);
             $this->logger->debug('Pruned file "' . $filePath . '"');
         }
+    }
+
+    private function pruneFileFromPath(string $path, string $fileName): void
+    {
+        $this->filesystem->remove($path . DIRECTORY_SEPARATOR . $fileName);
     }
 }
