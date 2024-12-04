415cce58b792a799146c732a00c321c5a005dc1e
Denis Zunke <denis.zunke@gmail.com>
2024-12-04 17:43:36 +0100

    Create test for document view

--- src/Document/Presentation/Controller/DocumentView.php
+++ src/Document/Presentation/Controller/DocumentView.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
-use Twig\Environment;
 
 #[Route(
     '/library/document/{document}',
@@ -19,16 +18,8 @@ use Twig\Environment;
 )]
 class DocumentView extends AbstractController
 {
-    public function __construct(
-        private readonly Environment $environment,
-    ) {
-    }
-
     public function __invoke(Request $request, Document $document): Response
     {
-        return new Response($this->environment->render(
-            'library/document_view.html.twig',
-            ['document' => $document],
-        ));
+        return $this->render('library/document_view.html.twig', ['document' => $document]);
     }
 }
