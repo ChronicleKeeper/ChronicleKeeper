910c9e9c1f52d56b5af499fe0181b9ac252b5011
Denis Zunke <denis.zunke@gmail.com>
2024-12-27 18:07:46 +0100

    Implement edit of user prompts

--- src/Settings/Presentation/Controller/ChangeSettings/SystemPromptEdit.php
+++ src/Settings/Presentation/Controller/ChangeSettings/SystemPromptEdit.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace ChronicleKeeper\Settings\Presentation\Controller\ChangeSettings;
 
+use ChronicleKeeper\Settings\Application\Command\StoreSystemPrompt;
 use ChronicleKeeper\Settings\Application\Service\SystemPromptRegistry;
+use ChronicleKeeper\Settings\Domain\Entity\SystemPrompt;
 use ChronicleKeeper\Settings\Presentation\Form\SystemPromptType;
+use ChronicleKeeper\Shared\Presentation\FlashMessages\Alert;
+use ChronicleKeeper\Shared\Presentation\FlashMessages\HandleFlashMessages;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+
+use function assert;
 
 #[Route(
     '/settings/chatbot_system_prompts/{id}',
@@ -19,15 +27,30 @@ use Symfony\Component\Routing\Requirement\Requirement;
 )]
 class SystemPromptEdit extends AbstractController
 {
+    use HandleFlashMessages;
+
     public function __construct(
         private readonly SystemPromptRegistry $systemPromptRegistry,
+        private readonly MessageBusInterface $bus,
     ) {
     }
 
-    public function __invoke(string $id): Response
+    public function __invoke(Request $request, string $id): Response
     {
         $systemPrompt = $this->systemPromptRegistry->getById($id);
         $form         = $this->createForm(SystemPromptType::class, $systemPrompt);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $systemPrompt = $form->getData();
+            assert($systemPrompt instanceof SystemPrompt); // Ensured by Form
+
+            $this->bus->dispatch(new StoreSystemPrompt($systemPrompt));
+
+            $this->addFlashMessage($request, Alert::SUCCESS, 'Der System Prompt wurde erfolgreich bearbeitet.');
+
+            return $this->redirectToRoute('settings_system_prompts');
+        }
 
         return $this->render(
             'settings/system_prompt_edit.html.twig',
--- tests/Settings/Presentation/Controller/ChangeSettings/SystemPromptEditTest.php
+++ tests/Settings/Presentation/Controller/ChangeSettings/SystemPromptEditTest.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace ChronicleKeeper\Test\Settings\Presentation\Controller\ChangeSettings;
 
+use ChronicleKeeper\Settings\Domain\ValueObject\SystemPrompt\Purpose;
 use ChronicleKeeper\Settings\Presentation\Controller\ChangeSettings\SystemPromptEdit;
+use ChronicleKeeper\Shared\Infrastructure\Persistence\Filesystem\Contracts\FileAccess;
+use ChronicleKeeper\Test\Settings\Domain\Entity\SystemPromptBuilder;
+use ChronicleKeeper\Test\Shared\Infrastructure\Persistence\Filesystem\FileAccessDouble;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Large;
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+
+use function assert;
+use function json_decode;
+use function json_encode;
+use function reset;
+
+use const JSON_THROW_ON_ERROR;
 
 #[CoversClass(SystemPromptEdit::class)]
 #[Large]
@@ -33,5 +44,49 @@ class SystemPromptEditTest extends WebTestCase
 
         // There is no save button on the readonly view
         self::assertStringNotContainsString('Speichern', $content);
+    }
+
+    #[Test]
+    public function itIsEditingASystemPrompt(): void
+    {
+        $fixturePrompt = (new SystemPromptBuilder())->asDefault()->build();
+
+        $client = static::createClient();
+
+        // Check the new System Prompt is stored
+        $fileAccess = $client->getContainer()->get(FileAccess::class);
+        assert($fileAccess instanceof FileAccessDouble);
+
+        $fileAccess->write(
+            'storage',
+            'system_prompts.json',
+            json_encode([$fixturePrompt->getId() => $fixturePrompt], JSON_THROW_ON_ERROR),
+        );
+
+        $client->request(
+            Request::METHOD_POST,
+            '/settings/chatbot_system_prompts/' . $fixturePrompt->getId(),
+            [
+                'system_prompt' => [
+                    'name' => 'Test System Prompt',
+                    'purpose' => Purpose::DOCUMENT_OPTIMIZER->value,
+                    'content' => 'Test Content',
+                ],
+            ],
+        );
+        self::assertResponseRedirects('/settings/chatbot_system_prompts');
+
+        $files = $fileAccess->allOfType('storage');
+        self::assertCount(1, $files);
+
+        $systemPrompt = reset($files);
+        $systemPrompt = json_decode($systemPrompt, true, 512, JSON_THROW_ON_ERROR);
+        $systemPrompt = reset($systemPrompt);
+
+        self::assertSame('Test System Prompt', $systemPrompt['name']);
+        self::assertSame('Test Content', $systemPrompt['content']);
+        self::assertSame(Purpose::DOCUMENT_OPTIMIZER->value, $systemPrompt['purpose']);
+        self::assertFalse($systemPrompt['isDefault']);
+        self::assertFalse($systemPrompt['isSystem']);
     }
 }
