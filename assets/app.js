9f7f3a55199d1339067716c9f4c2fb8364f50d04
Denis Zunke <denis.zunke@gmail.com>
2025-01-29 21:46:18 +0100

    fix: Modal loading from bootstrap js defects tabler js (#80)

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,9 @@
+// Vendors
+import './helpers/bootstrap.js';
+
+// Own Stuff
 import './bootstrap.js';
 import './css/app.css';
 import './css/messages.css';
+
+
--- importmap.php
+++ importmap.php
@@ -85,14 +85,18 @@ return [
         'version' => '2.4.1',
         'type' => 'css',
     ],
-    'bootstrap' => [
-        'version' => '5.3.3',
-    ],
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
     'bootstrap/dist/css/bootstrap.min.css' => [
         'version' => '5.3.3',
         'type' => 'css',
+    ],
+    '@tabler/core/dist/css/tabler.min.css' => [
+        'version' => '1.0.0',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.3',
     ],
 ];
