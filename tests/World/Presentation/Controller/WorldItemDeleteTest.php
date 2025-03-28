a049d03e3fc03910d1e40f4ad1359a18654ddccd
Denis Zunke <denis.zunke@gmail.com>
2025-03-28 21:32:06 +0100

    fix(World): fix deleting connected items in pgsql (#99)

--- tests/World/Presentation/Controller/WorldItemDeleteTest.php
+++ tests/World/Presentation/Controller/WorldItemDeleteTest.php
@@ -84,4 +84,51 @@ class WorldItemDeleteTest extends WebTestCase
         );
         self::assertEmpty($items);
     }
+
+    #[Test]
+    public function itCanDeleteAnItemWithRelations(): void
+    {
+        $anotherItem = (new ItemBuilder())->withName('Test Item 2')->build();
+
+        $this->databasePlatform->createQueryBuilder()->createInsert()
+            ->insert('world_items')
+            ->values([
+                'id' => $anotherItem->getId(),
+                'name' => $anotherItem->getName(),
+                'type' => $anotherItem->getType()->value,
+                'short_description' => $anotherItem->getShortDescription(),
+            ])
+            ->execute();
+
+        $this->databasePlatform->createQueryBuilder()->createInsert()
+            ->insert('world_item_relations')
+            ->values([
+                'relation_type' => 'related',
+                'source_world_item_id' => $this->item->getId(),
+                'target_world_item_id' => $anotherItem->getId(),
+            ])
+            ->execute();
+
+        $this->client->request(
+            Request::METHOD_GET,
+            '/world/item/' . $this->item->getId() . '/delete',
+            ['confirm' => 1],
+        );
+
+        self::assertResponseRedirects('/world');
+        $this->client->followRedirect();
+        self::assertSelectorTextContains('.alert-success', 'Der Eintrag wurde erfolgreich gelöscht.');
+
+        $relations = $this->databasePlatform->fetch(
+            'SELECT * FROM world_item_relations WHERE source_world_item_id = :id OR target_world_item_id = :id',
+            ['id' => $this->item->getId()],
+        );
+        self::assertEmpty($relations);
+
+        $relations = $this->databasePlatform->fetch(
+            'SELECT * FROM world_item_relations WHERE source_world_item_id = :id OR target_world_item_id = :id',
+            ['id' => $anotherItem->getId()],
+        );
+        self::assertEmpty($relations);
+    }
 }
