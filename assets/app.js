f6a14c76120512580720bc8d2323875ef77ecdde
Denis Zunke <denis.zunke@gmail.com>
2025-03-28 22:05:49 +0100

    fix(World): fix the relations autocomplete visualization

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,13 @@ import './helpers/bootstrap.js';
 
 // Own Stuff
 import './bootstrap.js';
+
+// Styles from vendors
+import 'tom-select/dist/css/tom-select.bootstrap5.css';
+
+// Styles from own stuff
 import './css/app.css';
 import './css/messages.css';
 import './css/calendar.css';
-
+import './css/tom-select.css';
 
